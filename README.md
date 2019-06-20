56cbbd4d37039043949344cb840e7f577b23b1bb
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-06-20 20:05:32 +0200

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 25.5
+## How to use: 
+1. Run `npm install`,
+2. Run `node server.js` to start.
+3. Go to `http://localhost:3000`.
+
+### Endpoint: 
+`/store`
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('assets'));
+
+app.use(function(req, res, next){
+    console.log('Hej, jestem pośrednikiem między żądaniem a odpowiedzią!');
+    next();
+});
+
+app.get('/', (req,res) => {
+    console.log('Otrzymałem żądanie GET');
+    res.sendFile('/index.html');
+});
+
+app.get('/userform', (req, res) => {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.json(response);
+});
+
+app.use('/store', (req, res, next) => {
+    console.log('Hej, jestem pośrednikiem przy żądaniu do /store');
+    next();
+})
+
+app.get('/store', (req, res) => {
+    res.send('To jest sklep');
+});
+
+const server = app.listen(3000, 'localhost', () => {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+});
